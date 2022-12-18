5157e5410bbd2eda5eba5872170f509bbcb3465e
Pravas Kumar Panda <106006353+Pravas-576@users.noreply.github.com>
2022-12-18 23:28:28 +0530

    Update and rename Code_ File to Code_ File.py

---
+++ Code_ File.py
@@ -0,0 +1,10 @@
+# Part-1 Data Preprocessing
+# Importing the Librarires
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the datasets
+dataset = pd.read_csv("Churn_Modelling.csv")
+dataset.head()
